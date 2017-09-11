f0cc3ceb1bd251536a4eca8e8fabe33cd2e0386c
Vitaminka-Lika <sarbassova_alika2001@mail.ru>
2017-09-11 21:45:35 +0600

    18/08/2017
    Классы

---
+++ ConsoleApp14/ConsoleApp14/Car.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    public class Car
+    {
+        private string mark;
+        private string model;
+        private string color;
+        private double volueOfCistern;
+        private int horsepower;
+
+        public void SetMark(string mark)
+        {
+            this.mark = mark;
+        }
+        public void SetModel(string model)
+        {
+            this.model = model;
+        }
+        public void SetColor(string color)
+        {
+            this.color = color;
+        }
+        public void SetVolueOfCistern(double volueOfCistern)
+        {
+            this.volueOfCistern = volueOfCistern;
+        }
+        public void SetHorsepower(int horsepower)
+        {
+            this.horsepower = horsepower;
+        }
+
+        public string GetMark()
+        {
+            return mark;
+        }
+        public string GetModel()
+        {
+            return model;
+        }
+        public string GetColor()
+        {
+            return color;
+        }
+        public double GetVolueOfCistern()
+        {
+            return volueOfCistern;
+        }
+        public int GetHorsepower()
+        {
+            return horsepower;
+        }
+    }
+}
