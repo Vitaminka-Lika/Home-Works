178e13cd5c92aeaf3b6019cff32a986db734d8be
Vitaminka-Lika <sarbassova_alika2001@mail.ru>
2017-09-18 21:28:22 +0600

    19/07/17
    Исключения

---
+++ ConsoleApp16/ConsoleApp16/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace ConsoleApp16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string path=Console.ReadLine();//"http://www.microsoft.com"
+                System.Diagnostics.Process.Start(path);
+            }
+            catch (Exception ex) {
+                Console.WriteLine("Ошибка: " + ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
