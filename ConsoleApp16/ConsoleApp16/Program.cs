dc53dcd300df68935f7991532f5285252ef0d9c8
Vitaminka-Lika <sarbassova_alika2001@mail.ru>
2017-09-19 21:31:30 +0600

    19/07/17 Заключительное исключение
    Исключения

--- ConsoleApp16/ConsoleApp16/Program.cs
+++ ConsoleApp16/ConsoleApp16/Program.cs
@@ -12,16 +12,48 @@ namespace ConsoleApp16
     {
         static void Main(string[] args)
         {
+            ToDoFirst();
+            Console.ReadLine();
+            ToDoSecond();
+        }
+
+        static void ToDoSecond()
+        {
+            try
+            {
+                Console.WriteLine("Введите размер массива");
+                int size;
+                string userInput = Console.ReadLine();
+                bool isOperationDone = Int32.TryParse(userInput, out size);
+                if (isOperationDone)
+                {
+                    int[] nums = new int[size];
+                    for (int i = 0; i <= size; i++)
+                    {
+                        nums[i] = i;
+                    }
+                }
+            }
+            catch(Exception ex) {
+                Console.WriteLine("Ошибка: " + ex.Message);
+                Console.WriteLine("Массив обработан");
+                Console.ReadLine();
+            }
+        }
+
+        static void ToDoFirst()
+        {
             try
             {
                 string path = Console.ReadLine();//"http://www.microsoft.com"
-                WebClient client=new WebClient();
+                WebClient client = new WebClient();
                 client.DownloadString(path);
                 Process.Start(path);
 
 
             }
-            catch (WebException ex) {
+            catch (WebException ex)
+            {
                 Console.WriteLine("Ошибка: " + ex.Message);
                 Console.ReadLine();
             }
@@ -31,5 +63,6 @@ namespace ConsoleApp16
             //    Console.ReadLine();
             //}
         }
+
     }
 }
