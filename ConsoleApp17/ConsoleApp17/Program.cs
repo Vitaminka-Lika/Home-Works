ef9744d4ec6aee6080acd190940d1ccaca7cffe7
Vitaminka-Lika <sarbassova_alika2001@mail.ru>
2017-09-19 20:48:25 +0600

    16/08/2017
    Индексаторы

---
+++ ConsoleApp17/ConsoleApp17/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество чисел");
+            int size;
+            string userInput = Console.ReadLine();
+            bool isOperationDone = Int32.TryParse(userInput, out size);
+            if (isOperationDone)
+            {
+                Console.WriteLine("Введите числа через Enter");
+                int[] nums = new int[size];
+                for (int i = 0; i < size; i++)
+                {
+                    int number;
+                    bool isOperationDone2 = Int32.TryParse(Console.ReadLine(), out number);
+                    if (isOperationDone2)
+                    {
+                        nums[i] = (int)Math.Pow(number, 2);
+                    }
+                    else {
+                        Console.WriteLine("Ошибка ввода");
+                        Console.ReadLine();
+                    }
+                }
+                for (int i = 0; i < size; i++)
+                {
+                    Console.WriteLine("Number"+i+" "+nums[i]);                   
+                }
+                Console.ReadLine();
+            }
+            else {
+                Console.WriteLine("Ошибка ввода");
+                Console.ReadLine();
+            }
+        }
+    }
+}
