0a99d371ab8704d85b1a171f55e43d89fb4b99d5
Vitaminka-Lika <sarbassova_alika2001@mail.ru>
2017-09-22 00:26:02 +0600

    31/08/17
    Рефлексия

---
+++ ConsoleApp4/ConsoleApp4/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    public class Book
+    {
+        public string Name { get; set; }
+
+        public double Cost { get; set; }
+        
+        public string Author { get; set; }
+
+        public int Year { get; set; }
+       
+    }
+}
---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ToDoFirst();
+            ToDoSecond();
+        }
+
+        static void ToDoFirst()
+        {
+            Type type = typeof(Console);
+            foreach (MethodInfo methodInfo in type.GetMethods())
+            {
+                ParameterInfo[] paramInfo = methodInfo.GetParameters();
+                for (int i = 0; i < paramInfo.Length; i++)
+                {
+                    Console.WriteLine(paramInfo[i].ParameterType.Name + ": " + paramInfo[i].ParameterType.Attributes);
+                }
+            }
+
+            Console.ReadLine();
+        }
+
+        static void ToDoSecond()
+        {
+            Book book = new Book()
+            {
+                Name = "Name",
+                Cost = 2840.7,
+                Author = "Author",
+                Year = 2012
+            };
+
+            Type type = typeof(Book);
+
+            foreach (PropertyInfo propertyInfo in type.GetProperties())
+            {
+                Console.WriteLine(propertyInfo.Name + " " + propertyInfo.GetValue(book));
+            }
+
+            Console.ReadLine();
+        }   
+    }
+}
